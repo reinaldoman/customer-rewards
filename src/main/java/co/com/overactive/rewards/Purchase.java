42993b5a55d6a8b6f16d337aa199626bd1f8926d
Reinaldo Otalvaro <reinaldo.otalvaro@overactive.com>
2021-11-16 10:29:05 -0500

    Creating repository an integration with database

--- src/main/java/co/com/overactive/rewards/Purchase.java
+++ src/main/java/co/com/overactive/rewards/Purchase.java
@@ -1,11 +1,12 @@
-package co.com.overactive.rewards;
-
-import lombok.Data;
-
-@Data
-public class Purchase {
-	
-	private Double value;
-	private Customer customer;
-
-}
+//package co.com.overactive.rewards;
+//
+//import javax.persistence.Entity;
+//
+//
+//@Entity
+//public class Purchase {
+//	
+//	private Double value;
+//	private Customer customer;
+//
+//}
---
+++ src/main/java/co/com/overactive/rewards/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package co.com.overactive.rewards.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import co.com.overactive.rewards.model.Customer;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+	List<Customer> findByAge(int age);
+}
