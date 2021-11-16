0dc8a2e1ad2dbe71a2ca985a3bd04b9d7fb27838
Reinaldo Otalvaro <reinaldo.otalvaro@overactive.com>
2021-11-16 08:32:45 -0500

    Adding Customer repository and test

--- src/main/java/co/com/overactive/rewards/Customer.java
+++ src/main/java/co/com/overactive/rewards/Customer.java
@@ -1,11 +1,42 @@
 package co.com.overactive.rewards;
 
-import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-@Data
+@Entity
 public class Customer {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
-	private String name;
-	
+	private String firstName;
+	private String lastName;
+
+	protected Customer() {}
+
+	public Customer(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return String.format(
+				"Customer[id=%d, firstName='%s', lastName='%s']",
+				id, firstName, lastName);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
 }
---
+++ src/main/java/co/com/overactive/rewards/CustomerRepository.java
@@ -0,0 +1,12 @@
+package co.com.overactive.rewards;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+	List<Customer> findByLastName(String lastName);
+
+	Customer findById(long id);
+}
